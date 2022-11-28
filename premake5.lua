bbc4961e46b6172e142266c986b81d9c7ec8c499
Nikita Kozlov <Nyarstot@protonmail.com>
2022-11-28 05:37:51 +0300

    setup premake5 to project

---
+++ premake5.lua
@@ -0,0 +1,115 @@
+workspace "RemaEngine"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}.%{cfg.system}.%{cfg.architecture}"
+
+project "RemaEngine"
+    location "RemaEngine"
+    kind "SharedLib"
+    language "C++"
+    toolset "v142"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/source/**.h",
+        "%{prj.name}/source/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/source",
+        "%{prj.name}/thirdparty/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "REMA_WIN",
+            "REMA_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{MKDIR} %{wks.location}bin/" .. outputdir .. "/SampleNovel/"),
+            ("{COPYFILE} %{cfg.buildtarget.abspath} %{wks.location}bin/" .. outputdir .. "/SampleNovel/%{cfg.buildtarget.name}")
+        }
+
+    filter "configurations:Debug"
+        defines "REMA_DEBUG"
+        buildoptions "/MDd"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "REMA_RELEASE"
+        buildoptions "/MD"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "REMA_DIST"
+        buildoptions "/MD"
+        optimize "On"
+
+project "SampleNovel"
+    location "SampleNovel"
+    kind "ConsoleApp"
+    language "C++"
+    toolset "v142"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/source/**.h",
+        "%{prj.name}/source/**.cpp"
+    }
+
+    includedirs
+    {
+        "RemaEngine/thirdparty/spdlog/include",
+        "RemaEngine/source"
+    }
+
+    links
+    {
+        "RemaEngine"
+    }
+
+    filter "system:windows"
+    cppdialect "C++17"
+    staticruntime "On"
+    systemversion "latest"
+
+    defines
+    {
+        "REMA_WIN"
+    }
+
+    filter "configurations:Debug"
+        defines "REMA_DEBUG"
+        buildoptions "/MDd"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "REMA_RELEASE"
+        buildoptions "/MD"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "REMA_DIST"
+        buildoptions "/MD"
+        optimize "On"
